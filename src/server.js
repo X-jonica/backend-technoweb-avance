ca666e9813aea8065e85484c3b526eec9d9bc488
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-06-20 19:42:14 +0300

    session expirer sur keyob pour de deployement du back donc re-deployement avec render

--- src/server.js
+++ src/server.js
@@ -7,7 +7,9 @@ const Panier = require("./models/Panier");
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, async () => {
-    console.log(`🚀 Serveur démarré sur https://grieving-tonie-x-jonica-0a1c8b87.koyeb.app`);
+    console.log(
+        `🚀 Serveur démarré sur https://backend-technoweb-avance.onrender.com`
+    );
     try {
         await db.sequelize.sync(); //creer la table si elle n existe pas encore
         console.log("📦 Base de données synchronisée");
